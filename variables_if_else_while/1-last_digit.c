abb41d2fc9bf840aabd9c1e0fd183209508f9121
tramiNGY <ngytrami@gmail.com>
2024-10-21 16:39:31 +0200

    Corrected mistype in 1-

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ if (n % 10 == 0)
 {
 printf("Last digit of %i is %i and is 0\n", n, n % 10);
 }
-if (10 < 6 && n % 10 != 0)
+if (n % 10 < 6 && n % 10 != 0)
 {
 printf("Last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
 }
