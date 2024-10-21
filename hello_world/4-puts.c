863bc7720992a1b5c377cdc89c2fa52359cca39d
tramiNGY <ngytrami@gmail.com>
2024-10-21 12:48:05 +0200

    Runned betty for 4-

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
-    int main(void)
-    {
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
-    }	
+/**
+ * main - prints char
+ * Return: return 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
