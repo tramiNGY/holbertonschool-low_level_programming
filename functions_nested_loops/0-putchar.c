672472acb68427e98c64b49697e7bcd03ce0e599
tramiNGY <ngytrami@gmail.com>
2024-10-22 15:38:16 +0200

    Changed 0- and main.h

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _putchar - print _putchar
+ * main - print _putchar
  * Return: returns 0
 */
-int _putchar(void)
-{	
-printf("_putchar\n");
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
 return (0);
 }
