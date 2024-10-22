035d1141f474d8b4a159e5a9711e20b5fb8cf8b8
tramiNGY <ngytrami@gmail.com>
2024-10-22 10:56:45 +0200

    Add 7-print_tebahpla

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main- print alphabet in reverse lowercase
+ * Return: returns 0
+*/
+int main(void)
+{
+int i;
+for (i = 122; i <= 97; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
