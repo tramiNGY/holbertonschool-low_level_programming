42a55270eb82283e079628d19d9169b51846940d
tramiNGY <ngytrami@gmail.com>
2024-10-22 15:28:48 +0200

    Added 0-putchar and main.h

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include "main.h"
 /**
- * main- print _putchar
+ * _putchar - print _putchar
  * Return: returns 0
 */
-int main(void)
-{
-printf("_putchar");
+int _putchar(void)
+{	
+printf("_putchar\n");
 return (0);
 }
