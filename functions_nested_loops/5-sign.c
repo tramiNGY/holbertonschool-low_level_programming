3e105968ab15db0ebea0982214cfc2816a043e27
tramiNGY <ngytrami@gmail.com>
2024-10-23 16:42:54 +0200

    Add 5-sign and updated main.h

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign - return sign of number
+ * @n: int number
+ * Return: returns 1 0 or -1
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{	
+_putchar('+');
+return (1);
+}
+if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+}
