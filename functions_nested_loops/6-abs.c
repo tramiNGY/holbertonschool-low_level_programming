2c2847d020862dfe80bddf709ec462bd2420ea35
tramiNGY <ngytrami@gmail.com>
2024-10-23 17:12:09 +0200

    Add 6-abs and updated main.h

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs - computes absolute value of an integer
+ * @int: integer
+ * Return: returns int
+*/
+int _abs(int)
+{
+int a;
+
+if (a >= 0)
+return (a);
+if (a < 0)
+return (-a);
+return (0);
+}
+
