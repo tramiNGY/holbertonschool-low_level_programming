3906e1ac89af9c40c79a48c53207acabe208d8c5
tramiNGY <ngytrami@gmail.com>
2024-10-23 10:09:57 +0200

    Add 2-print_alphabet_x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet lowercase 10 times
+*/
+void print_alphabet_x10(void)
+{
+int j;
+while (j <= 10)
+{
+j++;
+int i;
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+}
