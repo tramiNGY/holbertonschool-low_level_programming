bf0b842d3b6ea3a8f81582d1b78b3df5edece238
tramiNGY <ngytrami@gmail.com>
2024-10-23 15:06:06 +0200

    Add 3-islower and updated main.h

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * _islower - returns 1 if lowercase and 0 if not
+ * @c: character
+ * Return: returns 1 or 0
+*/
+
+int _islower(int c)
+{
+if (islower(c) == 1)
+return (1);
+else
+return (0);
+}
