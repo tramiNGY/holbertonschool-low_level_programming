b95a21f7bb74ff7e250a7f3de2b453f5c999cfd7
tramiNGY <ngytrami@gmail.com>
2024-10-24 17:04:01 +0200

    Modified 7-

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -9,17 +9,13 @@
 
 int print_last_digit(int r)
 {
-if (r == 0)
-	_putchar('0');
-else if (r > 0)
-	_putchar('0' + (r % 10));
-else if (r < 0)
+if (r < 0)
 {
 	r = r * (-1);
 	_putchar('0' + (r % 10));
 }
 else
-	_putchar('0' + r);
+	_putchar('0' + (r % 10));
 
 return (0);
 }
