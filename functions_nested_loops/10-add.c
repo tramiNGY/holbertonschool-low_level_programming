59d4b69bfdc56c9dd7fd1f03dfac757f8355059f
tramiNGY <ngytrami@gmail.com>
2024-10-26 09:51:31 +0200

    Corrected betty style in 10-

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * add - returns sum of int a and int b
+ * add- returns sum of int a and int b
+ * @a: int number
+ * @b: int number
  */
 int add(int a, int b)
 {
