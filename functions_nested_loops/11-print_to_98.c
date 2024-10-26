3cb5614cba344e2a7aa1ace6a2afba99b4bcb52c
tramiNGY <ngytrami@gmail.com>
2024-10-26 10:47:36 +0200

    Add 11-print_to_98 and updated main.h

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - prints all numbers from n to 98
+ * @n: int number
+*/
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+	}
+	if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+	else
+		printf("%d\n", n);
+}
