93b8b3c0ba5a8bf4fea95b22cb2e7eba0eb7c9eb
tramiNGY <ngytrami@gmail.com>
2024-10-26 10:01:37 +0200

    Forgot return description in 10-

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
- * add- returns sum of int a and int b
+ * add- sum of int a and int b
  * @a: int number
  * @b: int number
+ * Return: returns a+b
  */
+
 int add(int a, int b)
 {
 return (a + b);
