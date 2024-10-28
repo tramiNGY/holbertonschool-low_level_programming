67fe0e9575be8a21e2d19be9033da6f4f5520f3f
tramiNGY <ngytrami@gmail.com>
2024-10-28 15:45:40 +0100

    Add 8-print_square and updated main.h

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_square - prints a square followed by a new line
+ * @size: int square
+*/
+void print_square(int size)
+{
+int i;
+int j;
+
+for (i = 0; i < size; i++)
+{
+	for (j = 0; j < size; j++)
+	{
+		_putchar(35);
+	}
+_putchar('\n');
+}
+}
