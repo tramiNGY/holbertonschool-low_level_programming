f1c9461f08fbf0950d6c408e93a6a3174adfcc40
tramiNGY <ngytrami@gmail.com>
2024-10-28 21:35:30 +0100

    Add 5-more_numbers and updated main.h

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * more_numbers - prints 10 times numbers from 0 to 14
+*/
+
+void more_numbers(void)
+{
+int i = 0;
+int j = 0;
+
+	while (i < 10)
+	{
+		while (j <= 14)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+			else
+				_putchar(j + '0');
+			j++;
+		}
+	_putchar('\n');
+	i++;
+	j = 0;
+	}
+}
