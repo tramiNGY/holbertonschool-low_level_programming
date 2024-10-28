aae71e0c716e4ef70b5706b81db0f83224eac9dd
tramiNGY <ngytrami@gmail.com>
2024-10-28 16:07:43 +0100

    Add 9-fizz_buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints numbers from 1 to 100, x3 Fizz x5 Buzz x3x5 Fizzbuzz
+*/
+void main(void)
+{
+int i;
+printf("0 ");
+for (i = 1; i <= 100; i++)
+	if ((i % 3 == 0) && (i % 5 != 0))
+		printf("Fizz ");
+	else if ((i % 5 == 0) && (i % 3 != 0))
+		printf("Buzz ");
+	else if ((i % 3 == 0) && (i % 5 == 0))
+		printf("FizzBuzz ");
+	else
+		printf("%d ", i);
+}
