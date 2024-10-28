d26bbe68c41f1e8cb1ce9ed28b0816247e810ad4
tramiNGY <ngytrami@gmail.com>
2024-10-28 15:27:49 +0100

    Corrected betty style for 7-

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -3,8 +3,6 @@
 /**
  * print_diagonal - draws a diagonal line on the terminal
  * @n: int
- * @i: int
- * @j: int
 */
 
 void print_diagonal(int n)
@@ -14,7 +12,7 @@ int j;
 if (n > 0)
 {
 	for (i = 0; i < n; i++)
-	{			
+	{
 		for (j = 0; j < i; j++)
 		{
 			_putchar(' ');
