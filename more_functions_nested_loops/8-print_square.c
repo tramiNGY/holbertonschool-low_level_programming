63194693b91f3a788a8a425b143012dbc5eeb13f
tramiNGY <ngytrami@gmail.com>
2024-10-28 15:48:52 +0100

    Added condition in 8-

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,13 +8,17 @@ void print_square(int size)
 {
 int i;
 int j;
-
-for (i = 0; i < size; i++)
+if (size > 0)
 {
-	for (j = 0; j < size; j++)
+	for (i = 0; i < size; i++)
 	{
-		_putchar(35);
+		for (j = 0; j < size; j++)
+		{
+			_putchar(35);
+		}
+	_putchar('\n');
 	}
-_putchar('\n');
 }
+else
+_putchar('\n');
 }
