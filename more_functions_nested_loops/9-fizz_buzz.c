3d8bb3bc30e9ab0a888991c3a88432b50e650fd2
tramiNGY <ngytrami@gmail.com>
2024-10-28 16:11:56 +0100

    Deleted 0 in 9-

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - prints numbers from 1 to 100, x3 Fizz x5 Buzz x3x5 Fizzbuzz
+ * main- prints numbers from 1 to 100, x3 Fizz x5 Buzz x3x5 Fizzbuzz
+ * Return: returns 0
 */
 int  main(void)
 {
 int i;
-printf("0 ");
 for (i = 1; i <= 100; i++)
 	if ((i % 3 == 0) && (i % 5 != 0))
 		printf("Fizz ");
