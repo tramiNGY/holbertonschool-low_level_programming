9543e55ed5cf1e55081b52a82a00ccd066ac2a4c
tramiNGY <ngytrami@gmail.com>
2024-10-28 20:29:34 +0100

    Add 10-print_triangle and updated main.h

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_triangle - prints a triangle followed by a new line
+ * @size: int triangle size
+*/
+
+void print_triangle(int size)
+{
+int i;
+int j;
+int k;
+
+if (size > 0)
+{
+	for (i = 0; i < size; i++)
+	{
+		for (j = size; j > i; j--)
+		{
+			_putchar (' ');
+		}
+		for (k = 0; k <= i; k++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+}
+else
+_putchar('\n');
+}
