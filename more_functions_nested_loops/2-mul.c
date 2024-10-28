7f917538f6345426d063f9f9d2305c8c2407aaf8
tramiNGY <ngytrami@gmail.com>
2024-10-28 11:07:16 +0100

    Added 2-mul and updated main.h

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul- mutltiples two intergers
+ * @a: int
+ * @b: int
+ * Return: returns a*b
+*/
+
+int mul(int a, int b)
+{
+int multi;
+
+	multi = a * b;
+	return (multi);
+}
