d6abfd3fedfd287ebf115060335a4e104f2a9114
tramiNGY <ngytrami@gmail.com>
2024-10-29 21:09:11 +0100

    Add 7-puts_half and updated main.h

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts_half - prints second half of string
+ * @str: string
+*/
+
+void puts_half(char *str)
+{
+int i;
+int length;
+i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	length = i;
+
+	for (i = length / 2; i <= length; i++)
+		_putchar(str[i]);
+_putchar('\n');
+}
