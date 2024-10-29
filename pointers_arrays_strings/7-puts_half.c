8b0414c9947a1238296b040a241290f831bfd286
tramiNGY <ngytrami@gmail.com>
2024-10-29 23:27:41 +0100

    Modified 7-

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -14,8 +14,13 @@ i = 0;
 	while (str[i] != '\0')
 		i++;
 	length = i;
-
-	for (i = length / 2 - 1; i < length; i++)
-		_putchar(str[i]);
+	if (length % 2 == 0)
+	{
+		for (i = length / 2; i < length; i++)
+			_putchar(str[i]);
+	}
+	else
+		for (i = (length - 1) / 2; i < length; i++)
+			_putchar(str[i]);
 _putchar('\n');
 }
