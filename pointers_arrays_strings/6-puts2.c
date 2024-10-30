80ac4a6733e5b7164b5638ddef9e17ec152c53c3
tramiNGY <ngytrami@gmail.com>
2024-10-30 17:14:39 +0100

    Mistype 6-

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 #include "main.h"
-
-void puts2(char *str)
-int i;
-
-for (i = 0; i <#include <stdio.h>
-#include "main.h"
 /**
  * puts2 - prints every other character of a string
  * @str: string
