b87dbe8691da6e3b11567a668555b15838e722bf
tramiNGY <ngytrami@gmail.com>
2024-11-01 21:41:15 +0100

    Modified return value in 3-

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _strcmp - compare 2 strings
+ * _strcmp - compare 2 strings differences
  * @s1: string 1
  * @s2: string 2
- * Return: returns int
+ * Return: returns int diff between 2 ASCII values
 */
 
 int _strcmp(char *s1, char *s2)
@@ -15,8 +15,8 @@ for (i = 0; s1[i] != '\0'; i++)
 	if (s1[i] == s2[i])
 		comp = 0;
 	else if (s1[i] > s2[i])
-		return (15);
+		return (s1[i] - s2[i]);
 	else
-		return (-15);
+		return (s1[i] - s2[i]);
 return (comp);
 }
