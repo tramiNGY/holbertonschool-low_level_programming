3dc5901b86e19d9d9aad16fbe9947fffab387be6
tramiNGY <ngytrami@gmail.com>
2024-11-01 09:51:16 +0100

    Add 9-strcpy and updated main.h

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * *_strcpy - copies string pointed to buffer
+ * @dest: char
+ * @src: char
+ * Return: returns pointer
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+int i;
+
+for (i = 0; src[i] != '\0'; i++)
+	dest[i] = src[i];
+return dest;
+}
