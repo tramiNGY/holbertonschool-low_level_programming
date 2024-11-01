2f4c993d646e02dd01423914e0f8d9be90d61e43
tramiNGY <ngytrami@gmail.com>
2024-11-01 10:01:52 +0100

    Added strop condition in 9-

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -10,13 +10,20 @@
 char *_strcpy(char *dest, char *src)
 {
 int i;
+int length;
 
 i = 0;
+length = 0;
 
+while (src[length] != '\0')
+{
+        length++;
+}
 while (src[i] != '\0')
 {
-	dest[i] = src[i];
-	i++;
+	if (i <= length)
+		dest[i] = src[i];
+		i++;
 }
 dest[i + 1] = '\0';
 return (dest);
