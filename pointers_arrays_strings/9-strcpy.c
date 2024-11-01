381b372a61155d9c4c012d809e26348a88490328
tramiNGY <ngytrami@gmail.com>
2024-11-01 09:58:26 +0100

    Changed loop in 9-

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -11,8 +11,13 @@ char *_strcpy(char *dest, char *src)
 {
 int i;
 
-for (i = 0; src[i] != '\0'; i++)
+i = 0;
+
+while (src[i] != '\0')
+{
 	dest[i] = src[i];
+	i++;
+}
 dest[i + 1] = '\0';
 return (dest);
 }
