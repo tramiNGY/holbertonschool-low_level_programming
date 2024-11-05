dec8065ffa48be2424b54529fcb2a6b46ae8c70b
tramiNGY <ngytrami@gmail.com>
2024-11-05 20:07:21 +0100

    Modified 5-

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * *_strstr - finds 1st occurence of substring 2 in string 1
@@ -13,24 +14,30 @@ int i, j, firstoccur, condition;
 
 firstoccur = 0;
 
+if (needle[0] == '\0')
+{
+	return (haystack);
+}
+
 for (i = 0; haystack[i] != '\0'; i++)
 {
 	if (haystack[i] == needle[0])
 	{
 		firstoccur = i;
-		for (j = 0; needle[j] != '\0'; j++)
+		condition = 1;
+		j = 0;
+		while (needle[j] != '\0' && haystack[i + j] == needle[j])
 		{
-			if (haystack[i] == needle[j])
-			{
-				condition = 1;
-				i++;
-			}
-			else
-				condition = 0;
+				j++;
 		}
-		if (condition == 1)
-			return (haystack + firstoccur);
+		if (needle[j] == '\0')
+			condition = 1;
+
+		else
+			condition = 0;
 	}
+	if (condition == 1)
+		return (haystack + firstoccur);
 }
 return ("");
 }
