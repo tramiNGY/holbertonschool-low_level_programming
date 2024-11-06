9254dd95d50800e7bffb5b7171213da05b2c791e
tramiNGY <ngytrami@gmail.com>
2024-11-06 21:45:28 +0100

    Optimized access to array elements using pointers and using only one for loop

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -13,10 +13,10 @@ int i, sum1, sum2;
 sum1 = 0;
 sum2 = 0;
 
-for (i = 0; i < (size * size); i = i + size + 1)
-	sum1 = sum1 + *(a + i);
-for (i = 0; i <= (size * size) - size; i = i + size - 1)
-	sum2 = sum2 + *(a + i);
-
+for (i = 0; i < size; i++)
+{
+	sum1 = sum1 + *(a + i * size + i);
+	sum2 = sum2 + *(a + i * size + (size - i - 1));
+}
 printf("%d, %d\n", sum1, sum2);
 }
