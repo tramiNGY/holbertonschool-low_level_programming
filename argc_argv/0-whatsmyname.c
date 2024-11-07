7e062935610b8b20346e6a94fadce1d035368876
tramiNGY <ngytrami@gmail.com>
2024-11-08 00:04:18 +0100

    Add 0-whatsmyname and main.h

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main- prints its name followed by a new line
+ * @argc: not used argument count variable int
+ * @argv: argument vector array of string containing all arguments
+*/
+
+void main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+}
---
+++ argc_argv/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void main(int argc, char **argv);
+
+
+#endif
