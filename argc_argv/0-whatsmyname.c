d425fcacd1cafc3a88eb3e3edf1d5d86bb801586
tramiNGY <ngytrami@gmail.com>
2024-11-08 14:51:14 +0100

    Added condition no space after last argument

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -12,7 +12,11 @@ int main(int argc, char **argv)
 {
 int i;
 	for (i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
-
+	{
+		printf("%s ", argv[i]);
+		if (i == argc)
+			printf("%s", argv[i]);
+	}
+	printf("\n");
 return (0);	
 }
