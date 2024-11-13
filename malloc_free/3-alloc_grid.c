06a70d0f6c64b03a59d0bb2d67c03e92a71bcb72
tramiNGY <ngytrami@gmail.com>
2024-11-13 13:50:35 +0100

    Added free if one line is NULL

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -9,6 +9,9 @@
  * Return: returns NULL if fail
  * Remark: each line of grid is a pointer
  * so type of grid[i] is int*
+ * Remark2: Added free if one line is NULL
+ * to free all previous allocated lines
+ * then to free grid main array
 */
 
 int **alloc_grid(int width, int height)
@@ -27,8 +30,13 @@ for (i = 0; i < height; i++)
 	grid[i] = malloc(sizeof(int) * width);
 
 	if (grid[i] == NULL)
-		return (NULL);
+	{
+		for (j = 0; j < i; j++)
+			free(grid[j]);
 
+	free(grid);
+	return (NULL);
+	}
 	for (j = 0; j < width; j++)
 		grid[i][j] = 0;
 }
