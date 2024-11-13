a1217c4b2d2fb26a7cf11206984b66b7556e63f3
tramiNGY <ngytrami@gmail.com>
2024-11-13 10:56:22 +0100

    Deleted unnecessary save pointers and modified NULL string conditions

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -9,34 +9,30 @@
  * @s1: 1st string
  * @s2: 2nd string
  * Return: returns NULL if fails
+ * Remark: same as for 1-strdup, no need for pointers
+ * to save position if we calculte length by index
+ * Remark: if s1 or s2 NULL, instead of returning 
+ * the non-null one, make it empty string
 */
 
 char *str_concat(char *s1, char *s2)
 {
 int i, j, length1, length2;
-char *concat, *s1_save, *s2_save;
+char *concat;
 
-s1_save = s1;
-s2_save = s2;
+length1 = 0;
+length2 = 0;
 
 if (s1 == NULL)
-	return (s2);
+	s1 = "";
 if (s2 == NULL)
-	return (s1);
-if (s1 == NULL && s2 == NULL)
-	return (NULL);
+	s2 = "";
 
-while (*s1 != '\0')
-{
+while (s1[length1] != '\0')
 	length1++;
-	s1++;
-}
 
-while (*s2 != '\0')
-{
+while (s2[length2] != '\0')
 	length2++;
-	s2++;
-}
 
 concat = malloc(sizeof(char) * (length1 + length2 + 1));
 
@@ -44,10 +40,10 @@ if (concat == NULL)
 	return (NULL);
 
 for (i = 0; i < length1; i++)
-	concat[i] = s1_save[i];
+	concat[i] = s1[i];
 
 for (j = length1, i = 0; j < length1 + length2; j++, i++)
-	concat[j] = s2_save[i];
+	concat[j] = s2[i];
 
 concat[length1 + length2] = '\0';
 return (concat);
