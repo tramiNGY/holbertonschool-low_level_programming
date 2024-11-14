297abe39a69de9e3716b409fd6833515ad5410e0
tramiNGY <ngytrami@gmail.com>
2024-11-14 10:45:23 +0100

    Added structures typedef directory README and dog.h

---
+++ structures_typedef/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+structures typedef
---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - informations about dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner name of dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
+
