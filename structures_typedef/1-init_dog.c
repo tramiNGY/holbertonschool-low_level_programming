e4738ccdb9e96433315ca4a84df5dcae6f566434
tramiNGY <ngytrami@gmail.com>
2024-11-14 22:35:15 +0100

    Added condition in case NULL pointer

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -13,8 +12,10 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 
-d->name = name;
-d->age = age;
-d->owner = owner;
-
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
