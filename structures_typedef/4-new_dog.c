3524a218707f2e9236c72f5b949a577ff8fb46b2
tramiNGY <ngytrami@gmail.com>
2024-11-15 15:07:07 +0100

    Copy name and owner in memory

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -8,16 +8,32 @@
  * @age: dog age
  * @owner: dog owner name
  * Return: newdog struct
+ * Remark: Added copy of name and owner stored in a allocated memory
 */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-
+int length1, length2;
 dog_t *newdog;
+
+length1 = 0;
+length2 = 0;
+
+while (name[length1] != '\0')
+	length1++;
+
+while (owner[length2] != '\0')
+	length2++;
 
 newdog = malloc(sizeof(dog_t));
 
 if (newdog == NULL)
+	return (NULL);
+
+newdog->name = malloc(sizeof(char) * (length1 + 1));
+newdog->owner = malloc(sizeof(char) * (length2 + 1));
+
+if (newdog->name == NULL || newdog->owner == NULL)
 	return (NULL);
 
 newdog->name = name;
