11b288cb6e3e09221fb75c48494f6e5e40dc8f33
tramiNGY <ngytrami@gmail.com>
2024-11-18 15:33:35 +0100

    Add 2-int_index and updated function_pointers.h

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an int
+ * returns index of 1st element cmp function does not return 0
+ * @array: array of int
+ * @size: size of array
+ * @cmp: pointer to a function to be used to compare values
+ * Return: return -1 or index of element
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+
+if (size <= 0)
+	return (-1);
+
+for (i = 0; i < size; i++)
+{
+	if (cmp(array[i]) != 0)
+		return (i);
+
+}
+i = i + 1;
+if (i == size)
+	return (-1);
+
+return (0);
+}
