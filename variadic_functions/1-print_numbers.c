6b146c54a3831575da1bc812e7b2cb1b1295908d
tramiNGY <ngytrami@gmail.com>
2024-11-19 23:51:40 +0100

    Add 1-print_numbers and updated variadic_functions.h

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers in arguments
+ * @separator: string to be printed between numbers
+ * @n: number of intergers passed to the function
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list listnumb;
+unsigned int i;
+
+if (separator != NULL)
+{
+	va_start(listnumb, n);
+	for (i = 1; i < n; i++)
+	{
+		printf("%d", va_arg(listnumb, int));
+		printf("%s", separator);
+	}
+	printf("%d\n", va_arg(listnumb, int));
+	va_end(listnumb);
+}
+}
