e93061a43df83e60a0b9dffcf42fb29e98208094
tramiNGY <ngytrami@gmail.com>
2024-11-19 17:37:39 +0100

    Deleted else and only 3 if

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -24,18 +24,12 @@ if ((argv[2][0] == '/' || argv[2][0] == '%') && (atoi(argv[3]) == 0))
 	exit(100);
 }
 
-else
+if (get_op_func(argv[2]) == NULL)
 {
-	if (argv[2][0] == '+' || argv[2][0] == '-' || argv[2][0] == '*'
-			|| argv[2][0] == '/' || argv[2][0] == '%')
-	{
-		printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
-	}
-	else
-	{
-		printf("Error\n");
-		exit(99);
-	}
+	printf("Error\n");
+	exit(99);
 }
+
+printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 return (0);
 }
