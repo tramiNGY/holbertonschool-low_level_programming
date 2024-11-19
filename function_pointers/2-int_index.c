8915f90c31eaa2f76f6505ecc8ea8f19770c3a12
tramiNGY <ngytrami@gmail.com>
2024-11-19 18:37:18 +0100

    Moved NULL if block before for block

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -18,13 +18,14 @@ int i;
 if (size <= 0)
 	return (-1);
 
+if (array == NULL || cmp == NULL)
+	return (-1);
+
 for (i = 0; i < size; i++)
 {
 	if (cmp(array[i]) != 0)
 		return (i);
 }
-if (array == NULL || cmp == NULL)
-	return (-1);
 
 return (-1);
 }
