cdf683d164c2e163782ae184a396f67f53acdbc7
tramiNGY <ngytrami@gmail.com>
2024-12-06 11:44:40 +0100

    Back to original code because checker still not fixed

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@
 
 size_t print_list(const list_t *h)
 {
-int nodesnumber;
+size_t nodesnumber;
 nodesnumber = 0;
 
 	if (h == NULL)
@@ -18,10 +18,10 @@ nodesnumber = 0;
 
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
+		if (h->str != NULL)
 			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
 
 		h = h->next;
 		nodesnumber++;
