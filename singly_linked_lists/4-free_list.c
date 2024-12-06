e960a12424ccb353988682d633395c191f84cc2e
tramiNGY <ngytrami@gmail.com>
2024-12-06 10:27:38 +0100

    Added 4-free_list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the first node of the list
+*/
+
+void free_list(list_t *head)
+{
+list_t *tempnode;
+
+while (head != NULL)
+{
+	tempnode = head;
+	head = head->next;
+	free(tempnode->str);
+	free(tempnode);
+}
+}
