feee265e18b64baec6c4ddd1ab432a3440084f24
tramiNGY <ngytrami@gmail.com>
2024-12-08 12:02:33 +0100

    Added 1-dlisntint_len

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in a linked dlistint_t list
+ * @h: pointer to a dlistint_t structure
+ * Return: returns number of nodes
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+unsigned int nodesnumber;
+
+while (h != NULL)
+{
+	nodesnumber++;
+	h = h->next;
+}
+return (nodesnumber);
+}
