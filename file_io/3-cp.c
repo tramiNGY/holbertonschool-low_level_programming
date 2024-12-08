a0f63467cf0721fc11b47140e9a73e71db7e6153
tramiNGY <ngytrami@gmail.com>
2024-12-08 20:05:47 +0100

    Add 3-cp and updated header

---
+++ file_io/3-cp.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include "main.h"
+
+/**
+ * main - program that copies the content of a file to another file
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: returns 0 if success
+*/
+
+int main(int argc, char **argv)
+{ int file_from, file_to, lettersread, lettersprint;
+char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	} file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	} file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+		close(file_from);
+		exit(99);
+	} while ((lettersread = read(file_from, buffer, sizeof(buffer))) > 0)
+	{
+		lettersprint = write(file_to, buffer, lettersread);
+		if (lettersprint == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+			close(file_from);
+			close(file_to);
+			exit(99);
+		}
+	}
+	if (lettersread == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(file_from);
+		close(file_to);
+		exit(98);
+	}
+	if (close(file_from) == -1 || close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	} return (0);
+}
