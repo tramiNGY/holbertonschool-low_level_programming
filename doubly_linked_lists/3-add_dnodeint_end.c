e7eb1bb53cba3555c2caf3c2438d30fb89564f08
tramiNGY <ngytrami@gmail.com>
2024-12-08 12:27:54 +0100

    Add 3-add_dnodeint_end

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - adds a new node at the end ot dlistint_t list
+ * @head: pointer to pointer to the first node of the list
+ * @n: integer data of the node
+ * Return: returns the address of the new element or NULL if failed
+*/
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+dlistint_t *newnode;
+dlistint_t *tempnode;
+
+newnode = malloc(sizeof(dlistint_t));
+	if (newnode == NULL)
+		return (NULL);
+newnode->n = n;
+newnode->next = NULL;
+newnode->prev = NULL;
+
+if (*head == NULL)
+	*head = newnode;
+
+else
+{
+	tempnode = *head;
+	while (tempnode->next != NULL)
+		tempnode = tempnode->next;
+	tempnode->next = newnode;
+	tempnode->prev = tempnode;
+}
+return (newnode);
+}
